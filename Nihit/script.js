e37b2fe5a5aa3b73699398cc0e1707e627b599e4
Nihits18 <166545827+Nihits18@users.noreply.github.com>
2024-04-11 01:06:16 +0530

    Update script.js

--- Nihit/script.js
+++ Nihit/script.js
@@ -1,3 +1,19 @@
+//tab switches
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+
 //navbar hamburger
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
